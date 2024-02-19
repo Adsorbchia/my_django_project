f83e2329965715a624ac2dd9addeef1ee2d81f6a
Natalya <117934761+Adsorbchia@users.noreply.github.com>
2024-02-19 18:08:22 +0400

    add new file

--- my_django_project/myapp2/forms.py
+++ my_django_project/myapp2/forms.py
@@ -3,8 +3,6 @@ from .models import Product
 
 
 class ImageForm(forms.ModelForm):
-    # picture = forms.ImageField()
-
     class Meta:
         model = Product
         fields = ('product_name', 'picture')
